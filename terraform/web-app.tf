57b02494f65e587320582568021c835b80334a00
Fraser Molyneux <fraser.molyneux@hotmail.co.uk>
2020-03-27 08:30:27 +0000

    Updating interpolation option

--- terraform/web-app.tf
+++ terraform/web-app.tf
@@ -5,8 +5,8 @@ resource "azurerm_resource_group" "resource-group" {
 
 resource "azurerm_app_service_plan" "app-service-plan" {
     name = "FM-GeoLocation-Web-AppPlan-${var.environment}"
-    resource_group_name = "${azurerm_resource_group.resource-group.name}"
-    location = "${azurerm_resource_group.resource-group.location}"
+    resource_group_name = azurerm_resource_group.resource-group.name
+    location = azurerm_resource_group.resource-group.location
 
     sku {
         tier = "Shared"
@@ -16,13 +16,13 @@ resource "azurerm_app_service_plan" "app-service-plan" {
 
 resource "azurerm_app_service" "app-service" {
   name = "FM-GeoLocation-WebApp-${var.environment}"
-  location = "${var.region}"
-  resource_group_name = "${azurerm_resource_group.resource-group.name}"
-  app_service_plan_id = "${azurerm_app_service_plan.app-service-plan.id}"
+  location = var.region
+  resource_group_name = azurerm_resource_group.resource-group.name
+  app_service_plan_id = azurerm_app_service_plan.app-service-plan.id
 }
 
 resource "azurerm_app_service_custom_hostname_binding" "geo-location-net" {
   hostname = "geo-location.net"
-  app_service_name = "${azurerm_app_service.app-service.name}"
-  resource_group_name = "${azurerm_resource_group.resource-group.name}"
+  app_service_name = azurerm_app_service.app-service.name
+  resource_group_name = azurerm_resource_group.resource-group.name
 }
